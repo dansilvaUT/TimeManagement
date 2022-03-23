ce3abb5db53ae0c3828332606f1ebabbc689cb8c
dansilvaUT <dan13silv@gmail.com>
2022-03-23 15:26:52 -0600

    Updated models and seeded db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# User.create(username: "Test", email: "this@test.com", password: "password")
+
+# tasks = Task.create([{
+#     title: "seed",
+#     description: "description seed",
+#     start_time: "01:00PM",
+#     end_time: "02:00PM",
+#     user_id: 4
+# }])
